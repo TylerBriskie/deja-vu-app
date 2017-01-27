f0f71cdc430d41ea99a8e2822e926e3ac1630fe4
Michael Roque <mikeroque1@gmail.com>
2017-01-27 15:39:01 -0700

    added images

--- seeds/01_original.js
+++ seeds/01_original.js
@@ -1,6 +1,6 @@
 exports.seed = function(knex, Promise) {
     // Deletes ALL existing entries
-    return knex.raw("DELETE from original; ALTER SEQUENCE original_id_seq RESTART WITH 13")
+    return knex.raw("DELETE from original; ALTER SEQUENCE originals_id_seq RESTART WITH 13")
         .then(function() {
             return Promise.all([
                 // Inserts seed entries
@@ -8,73 +8,85 @@ exports.seed = function(knex, Promise) {
                     id: 1,
                     title: 'Annie',
                     year: 1982,
-                    type: 'Film'
+                    type: 'Film',
+                    image_url: "https://images-na.ssl-images-amazon.com/images/M/MV5BMTczMjgwNTAyNV5BMl5BanBnXkFtZTcwMDU3MzMyMQ@@._V1_.jpg"
                 }),
                 knex('original').insert({
                     id: 2,
                     title: 'The Bad News Bears',
                     year: 1976,
-                    type: 'Film'
+                    type: 'Film',
+                    image_url: "https://images-na.ssl-images-amazon.com/images/M/MV5BNTU1ZjNmNjktNjk4Yy00MzdjLTk0YzktMDRlYTllMjE0N2I0XkEyXkFqcGdeQXVyMTQxNzMzNDI@._V1_.jpg"
                 }),
                 knex('original').insert({
                     id: 3,
                     title: 'Carrie',
                     year: 1976,
-                    type: 'Film'
+                    type: 'Film',
+                    image_url: "https://images-na.ssl-images-amazon.com/images/M/MV5BN2Q3ZmVmZWItZGIzNi00NDNhLThiMTktMTU3MjIzMzRmYWI5XkEyXkFqcGdeQXVyMTQxNzMzNDI@._V1_.jpg"
                 }),
                 knex('original').insert({
                     id: 4,
                     title: 'Dawn of the Dead',
                     year: 1978,
-                    type: 'Film'
+                    type: 'Film',
+                    image_url: "https://images-na.ssl-images-amazon.com/images/M/MV5BMzc1YTIyNjctYzhlNy00ZmYzLWI2ZWQtMzk4MmQwYzA0NGQ1XkEyXkFqcGdeQXVyMTQxNzMzNDI@._V1_.jpg"
                 }),
                 knex('original').insert({
                     id: 5,
                     title: 'Freaky Friday',
                     year: 1976,
-                    type: 'Film'
+                    type: 'Film',
+                    image_url: "https://images-na.ssl-images-amazon.com/images/M/MV5BMzlmMjVjZTMtNWZmNC00ZDU3LWFkZmEtOTRkMWJhYTc3OTUyL2ltYWdlL2ltYWdlXkEyXkFqcGdeQXVyMTQxNzMzNDI@._V1_SY1000_CR0,0,640,1000_AL_.jpg"
                 }),
                 knex('original').insert({
                     id: 6,
                     title: 'The Italian Job',
                     year: 1969,
-                    type: 'Film'
+                    type: 'Film',
+                    image_url: "https://images-na.ssl-images-amazon.com/images/M/MV5BYzZjMWVlZGYtMmVkOC00NWY1LTg3MjEtZWRlM2RmMDE3ZmRhXkEyXkFqcGdeQXVyNDk3NzU2MTQ@._V1_SY1000_CR0,0,666,1000_AL_.jpg"
                 }),
                 knex('original').insert({
                     id: 7,
                     title: 'King Kong',
                     year: 1933,
-                    type: 'Film'
+                    type: 'Film',
+                    image_url: "https://images-na.ssl-images-amazon.com/images/M/MV5BODM0MTEyMjQxOF5BMl5BanBnXkFtZTgwNzYwNTk1MjE@._V1._CR8.883331298828125,6.5333404541015625,344,488_.jpg"
                 }),
                 knex('original').insert({
                     id: 8,
                     title: '21 Jump Street',
                     year: 1987,
-                    type: 'TV'
+                    type: 'TV',
+                    image_url: "https://images-na.ssl-images-amazon.com/images/M/MV5BMjExODMxNzMxMl5BMl5BanBnXkFtZTcwOTYxNDQzMQ@@._V1_.jpg"
                 }),
                 knex('original').insert({
                     id: 9,
                     title: 'Hercules',
                     year: 1998,
-                    type: 'TV'
+                    type: 'TV',
+                    image_url: "https://images-na.ssl-images-amazon.com/images/M/MV5BYmQxOTZkN2QtYjg4ZS00MWFlLWEwZDMtODMyZjZmZTI4ZTVhL2ltYWdlL2ltYWdlXkEyXkFqcGdeQXVyMjUwNzU0Njk@._V1_SY1000_SX1000_AL_.jpg"
                 }),
                 knex('original').insert({
                     id: 10,
                     title: 'Dragnet',
                     year: 1951,
-                    type: 'TV'
+                    type: 'TV',
+                    image_url: "https://images-na.ssl-images-amazon.com/images/M/MV5BMjAyNzgwNTQ2Nl5BMl5BanBnXkFtZTYwMzAwNDQ2._V1_.jpg"
                 }),
                 knex('original').insert({
                     id: 11,
                     title: 'RoboCop',
                     year: 1987,
-                    type: 'Film'
+                    type: 'Film',
+                    image_url: "https://images-na.ssl-images-amazon.com/images/M/MV5BZWVlYzU2ZjQtZmNkMi00OTc3LTkwZmYtZDVjNmY4OWFmZGJlXkEyXkFqcGdeQXVyMTQxNzMzNDI@._V1_SY1000_CR0,0,644,1000_AL_.jpg"
                 }),
                 knex('original').insert({
                     id: 12,
                     title: 'Point Break',
                     year: 1991,
-                    type: 'Film'
+                    type: 'Film',
+                    image_url: "https://images-na.ssl-images-amazon.com/images/M/MV5BNWVjZWFmYjItZGJlOC00YTllLWE4YjctMWY2ZTg5ZjE0MDIyXkEyXkFqcGdeQXVyNDk3NzU2MTQ@._V1_SY1000_SX675_AL_.jpg"
                 })
             ]);
         });
--- seeds/02_remake.js
+++ seeds/02_remake.js
@@ -10,13 +10,16 @@ exports.seed = function(knex, Promise) {
             title: 'Annie',
             year: 2014,
             type: 'Film',
+            image_url: "https://images-na.ssl-images-amazon.com/images/M/MV5BMTAyNDYxOTI4MzNeQTJeQWpwZ15BbWU4MDQ3MDU3NjIx._V1_SX300.jpg",
             orig_id: 1
+
         }),
         knex('remake').insert({
             id: 2,
             title: 'The Bad News Bears',
             year: 2005,
             type: 'Film',
+            image_url: "https://images-na.ssl-images-amazon.com/images/M/MV5BMzMzZDU1MTEtMDMzYy00MDhkLTg5MGMtYjFkNzYyMTc3YmU0XkEyXkFqcGdeQXVyMTQxNzMzNDI@._V1_.jpg",
             orig_id: 2
         }),
         knex('remake').insert({
@@ -24,6 +27,7 @@ exports.seed = function(knex, Promise) {
             title: 'Carrie',
             year: 2013,
             type: 'Film',
+            image_url: "https://images-na.ssl-images-amazon.com/images/M/MV5BODg2MDU2MjYxNl5BMl5BanBnXkFtZTcwMDQ5MzU0OQ@@._V1_.jpg",
             orig_id: 3
         }),
         knex('remake').insert({
@@ -31,6 +35,7 @@ exports.seed = function(knex, Promise) {
             title: 'Dawn of the Dead',
             year: 2004,
             type: 'Film',
+            image_url: "https://images-na.ssl-images-amazon.com/images/M/MV5BMTI4OTU2NjY5OF5BMl5BanBnXkFtZTYwOTQ2Mzg3._V1_.jpg",
             orig_id: 4
         }),
         knex('remake').insert({
@@ -38,6 +43,7 @@ exports.seed = function(knex, Promise) {
             title: 'Freaky Friday',
             year: 2003,
             type: 'Film',
+            image_url: "https://images-na.ssl-images-amazon.com/images/M/MV5BYmU4NTk4OWYtMjE4My00MGVkLTgwY2EtZTZjN2YyOGFiMDQ0L2ltYWdlL2ltYWdlXkEyXkFqcGdeQXVyMTQxNzMzNDI@._V1_SY1000_CR0,0,673,1000_AL_.jpg",
             orig_id: 5
         }),
         knex('remake').insert({
@@ -45,6 +51,7 @@ exports.seed = function(knex, Promise) {
             title: 'The Italian Job',
             year: 2003,
             type: 'Film',
+            image_url: "https://images-na.ssl-images-amazon.com/images/M/MV5BNDYyNzYxNjYtNmYzMC00MTE0LWIwMmYtNTAyZDBjYTIxMTRhXkEyXkFqcGdeQXVyNDk3NzU2MTQ@._V1_SY1000_CR0,0,666,1000_AL_.jpg",
             orig_id: 6
         }),
         knex('remake').insert({
@@ -52,6 +59,7 @@ exports.seed = function(knex, Promise) {
             title: 'King Kong',
             year: 2005,
             type: 'Film',
+            image_url: "https://images-na.ssl-images-amazon.com/images/M/MV5BMjYxYmRlZWYtMzAwNC00MDA1LWJjNTItOTBjMzlhNGMzYzk3XkEyXkFqcGdeQXVyMTQxNzMzNDI@._V1_SY1000_CR0,0,675,1000_AL_.jpg",
             orig_id: 7
         }),
         knex('remake').insert({
@@ -59,6 +67,7 @@ exports.seed = function(knex, Promise) {
             title: '21 Jump Street',
             year: 2012,
             type: 'Film',
+            image_url: "https://images-na.ssl-images-amazon.com/images/M/MV5BMTc3NzQ3OTg3NF5BMl5BanBnXkFtZTcwMjk5OTcxNw@@._V1_.jpg",
             orig_id: 8
         }),
         knex('remake').insert({
@@ -66,6 +75,7 @@ exports.seed = function(knex, Promise) {
             title: 'Hercules',
             year: 2014,
             type: 'Film',
+            image_url: "https://images-na.ssl-images-amazon.com/images/M/MV5BMTQ4ODA5MTA4OF5BMl5BanBnXkFtZTgwNjMyODM5MTE@._V1_SY1000_CR0,0,674,1000_AL_.jpg",
             orig_id: 9
         }),
         knex('remake').insert({
@@ -73,6 +83,7 @@ exports.seed = function(knex, Promise) {
             title: 'Dragnet',
             year: 1987,
             type: 'Film',
+            image_url: "https://images-na.ssl-images-amazon.com/images/M/MV5BMTUyOTMzNjk0M15BMl5BanBnXkFtZTcwNzc5MjUyMQ@@._V1_.jpg",
             orig_id: 10
         }),
         knex('remake').insert({
@@ -80,6 +91,7 @@ exports.seed = function(knex, Promise) {
             title: 'RoboCop',
             year: 2014,
             type: 'Film',
+            image_url: "https://images-na.ssl-images-amazon.com/images/M/MV5BMjAyOTUzMTcxN15BMl5BanBnXkFtZTgwMjkyOTc1MDE@._V1_SY1000_CR0,0,674,1000_AL_.jpg",
             orig_id: 11
         }),
         knex('remake').insert({
@@ -87,6 +99,7 @@ exports.seed = function(knex, Promise) {
             title: 'Point Break',
             year: 2015,
             type: 'Film',
+            image_url: "https://images-na.ssl-images-amazon.com/images/M/MV5BMjIxNDkzOTAyNV5BMl5BanBnXkFtZTgwNjEyOTY3NjE@._V1_SY1000_CR0,0,674,1000_AL_.jpg",
             orig_id: 12
         })
       ]);
